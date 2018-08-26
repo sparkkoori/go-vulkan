913703f947224bdcceed39b9935294522cd68d9b
koori <sk.koori@gmail.com>
2018-08-26 19:52:26 +0800

    Add v1.1

---
+++ v1.1/vk/build.go
@@ -0,0 +1,12 @@
+package glfw
+
+/*
+//Windows:
+#cgo windows CFLAGS: -DVK_USE_PLATFORM_WIN32_KHR
+#cgo windows LDFLAGS: -lvulkan-1
+
+//Darwin:
+#cgo darwin CFLAGS: -DVK_USE_PLATFORM_MACOS_MVK
+#cgo darwin LDFLAGS: -lvulkan.1
+*/
+import "C"
