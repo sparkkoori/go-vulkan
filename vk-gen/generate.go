73b55d7c1ff32b06e075b19117795abdf922d9a3
koori <sk.koori@gmail.com>
2018-09-11 21:46:27 +0800

    Gen enums

--- vk-gen/generate.go
+++ vk-gen/generate.go
@@ -79,6 +79,9 @@ func (ws *workspace) init(nodes []cast.Node) {
 			panic("Name conflict for " + name)
 		}
 		ws.source[name] = node
+		// if name == "enum VkResult" {
+		// 	ws.halt("", node)
+		// }
 	}
 }
 
@@ -102,15 +105,12 @@ func (ws *workspace) gen(name string) string {
 		goname = ws.genTypedefDecl(n, name)
 	// case *cast.RecordDecl:
 	// 	return "struct " + n.Name
-	// case *cast.EnumDecl:
-	// 	return "enum " + n.Name
+	case *cast.EnumDecl:
+		goname = ws.genEnumDecl(n, name)
 	// case *cast.FunctionDecl:
 	// 	return n.Name
 	default:
-		println("Unkown node for gen():")
-		deepPrint(node, 0)
-		ws.invalid = true
-		return ""
+		ws.halt("Unkown node for gen()", node)
 	}
 
 	ws.mapping[name] = goname
@@ -120,6 +120,14 @@ func (ws *workspace) gen(name string) string {
 func (ws *workspace) genTypedefDecl(node *cast.TypedefDecl, cn string) string {
 	ct := node.Type
 	gt := ws.gen(ct)
+
+	if gt != "" {
+		if strings.HasPrefix(ct, "enum ") ||
+			strings.HasPrefix(ct, "struct ") {
+			return gt //two-one mapping
+		}
+	}
+
 	if gt == "" {
 		gt = "C." + cn
 	}
@@ -137,6 +145,120 @@ func (ws *workspace) genTypedefDecl(node *cast.TypedefDecl, cn string) string {
 	return gn
 }
 
+func (ws *workspace) genEnumDecl(node *cast.EnumDecl, cn string) string {
+	gn := strings.TrimPrefix(cn, "enum ")
+	gn = vkName(gn)
+	ws.target.addGo(&goast.GenDecl{
+		Tok: token.TYPE,
+		Specs: []goast.Spec{
+			&goast.TypeSpec{
+				Name: &goast.Ident{token.NoPos, gn, nil},
+				Type: &goast.Ident{token.NoPos, "int", nil},
+			},
+		},
+	})
+
+	specs := []goast.Spec{}
+	constMapping := map[string]string{}
+	for _, child := range node.ChildNodes {
+		if ws.invalid {
+			continue
+		}
+		con := child.(*cast.EnumConstantDecl)
+
+		var expr = convExpr(constMapping, con.ChildNodes[0])
+
+		// ws.halt("Unkown enum const", v)
+		if expr != nil {
+			gcon := vkName(con.Name)
+			specs = append(specs, &goast.ValueSpec{
+				Names: []*goast.Ident{
+					&goast.Ident{Name: gcon},
+				},
+				Type:   &goast.Ident{Name: gn},
+				Values: []goast.Expr{expr},
+			})
+			constMapping[con.Name] = gcon
+		}
+	}
+
+	ws.target.addGo(&goast.GenDecl{
+		Tok:    token.CONST,
+		Lparen: token.Pos(1),
+		Specs:  specs,
+		Rparen: token.Pos(1),
+	})
+
+	return gn
+}
+
+func (ws *workspace) halt(str string, node cast.Node) {
+	if !ws.invalid {
+		ws.invalid = true
+		fmt.Println(aurora.Red("Generating Halted:"), str)
+		deepPrint(node, 0)
+	}
+}
+
+func convExpr(refs map[string]string, node cast.Node) goast.Expr {
+	switch v := node.(type) {
+	case *cast.IntegerLiteral:
+		return &goast.BasicLit{Kind: token.INT, Value: v.Value}
+	case *cast.DeclRefExpr:
+		return &goast.Ident{Name: refs[v.Name]}
+	case *cast.ParenExpr:
+		return &goast.ParenExpr{
+			Lparen: token.Pos(1),
+			X:      convExpr(refs, v.ChildNodes[0]),
+			Rparen: token.Pos(1),
+		}
+	case *cast.BinaryOperator:
+		return &goast.BinaryExpr{
+			X:  convExpr(refs, v.ChildNodes[0]),
+			Op: convToken(v.Operator),
+			Y:  convExpr(refs, v.ChildNodes[1]),
+		}
+	case *cast.UnaryOperator:
+		return &goast.UnaryExpr{
+			X:  convExpr(refs, v.ChildNodes[0]),
+			Op: convToken(v.Operator),
+		}
+	default:
+		return nil
+	}
+}
+
+func convToken(op string) token.Token {
+	switch op {
+	case token.ADD.String():
+		return token.ADD
+	case token.SUB.String():
+		return token.SUB
+	case token.MUL.String():
+		return token.MUL
+	case token.QUO.String():
+		return token.QUO
+	case token.REM.String():
+		return token.REM
+
+	case token.AND.String():
+		return token.AND
+	case token.OR.String():
+		return token.OR
+	case token.XOR.String():
+		return token.XOR
+	case token.SHL.String():
+		return token.SHL
+	case token.SHR.String():
+		return token.SHR
+	case token.AND_NOT.String():
+		return token.AND_NOT
+
+	default:
+		return token.ILLEGAL
+	}
+}
+
 func getNodeName(node cast.Node) string {
 	switch n := node.(type) {
 	case *cast.TypedefDecl:
@@ -148,8 +270,7 @@ func getNodeName(node cast.Node) string {
 	case *cast.FunctionDecl:
 		return n.Name
 	}
-	deepPrint(node, 0)
-	panic("Unkown node for getNodeName()")
+	panic("Unkown node name for " + reflect.TypeOf(node).String())
 	return ""
 }
 
