cfbcca794f052c0c7f9fd01b139de809ec3825a0
koori <sk.koori@gmail.com>
2018-09-30 12:21:00 +0800

    Change to MAKE_VERSION()

--- v1.1/vk/consts.go
+++ v1.1/vk/consts.go
@@ -1,19 +1,19 @@
 package vk
 
-func MakeVersion(major, minor, patch uint32) uint32 {
+func MAKE_VERSION(major, minor, patch uint32) uint32 {
 	return (((major) << 22) | ((minor) << 12) | (patch))
 }
 
-func VersionMajor(version uint32) uint32 {
+func VERSION_MAJOR(version uint32) uint32 {
 	return version >> 22
 }
 
-func VersionMinor(version uint32) uint32 {
+func VERSION_MINOR(version uint32) uint32 {
 	return (version >> 12) & 0x3ff // 10 bits
 }
 
-func VersionPatch(version uint32) uint32 {
+func VERSION_PATCH(version uint32) uint32 {
 	return version & 0xfff // 12 bits
 }
 
-const HeaderVersion uint32 = 82
+const HEADER_VERSION uint32 = 82
