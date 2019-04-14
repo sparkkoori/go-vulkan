472bd5734282edc2bfc38210d3a497527f853d8b
sparkkoori <sk.koori@gmail.com>
2019-04-14 16:55:40 +0800

    Add common consts

--- v1.1/vk/consts.go
+++ v1.1/vk/consts.go
@@ -16,4 +16,20 @@ func VERSION_PATCH(version uint32) uint32 {
 	return version & 0xfff // 12 bits
 }
 
-const HEADER_VERSION uint32 = 82
+const HEADER_VERSION = 82
+const LOD_CLAMP_NONE = 1000.0
+const REMAINING_MIP_LEVELS = ^uint32(0)
+const REMAINING_ARRAY_LAYERS = ^uint32(0)
+const WHOLE_SIZE = ^uint64(0)
+const ATTACHMENT_UNUSED = ^uint32(0)
+
+// const TRUE                        =    1
+// const FALSE                       =    0
+const QUEUE_FAMILY_IGNORED = ^uint32(0)
+const SUBPASS_EXTERNAL = ^uint32(0)
+const MAX_PHYSICAL_DEVICE_NAME_SIZE = 256
+const UUID_SIZE = 16
+const MAX_MEMORY_TYPES = 32
+const MAX_MEMORY_HEAPS = 16
+const MAX_EXTENSION_NAME_SIZE = 256
+const MAX_DESCRIPTION_SIZE = 256
